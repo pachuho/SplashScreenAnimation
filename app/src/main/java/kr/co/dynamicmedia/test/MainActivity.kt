d9e78482065cbb139262289e2dc5515264eecc1f
박주호 <blueblack96@dynamicmedia.co.kr>
2022-02-16 16:34:01 +0900

    init

---
+++ app/src/main/java/kr/co/dynamicmedia/test/MainActivity.kt
@@ -0,0 +1,66 @@
+package kr.co.dynamicmedia.test
+
+import android.animation.ObjectAnimator
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.AnticipateInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import androidx.core.animation.doOnEnd
+
+class MainActivity : AppCompatActivity() {
+    private val viewModel by viewModels<MainViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Set up an OnPreDrawListener to the root view.
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    // Check if the initial data is ready.
+                    return if (viewModel.complete.value == true) {
+                        // The content is ready; start drawing.
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        // The content is not ready; suspend.
+                        false
+                    }
+                    return false
+                }
+            }
+        )
+
+        Handler(mainLooper).postDelayed({
+            viewModel.updateComplete()
+        },500)
+
+//        // Add a callback that's called when the splash screen is animating to
+//        // the app content.
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+//            splashScreen.setOnExitAnimationListener { splashScreenView ->
+//                // Create your custom animation.
+//                val slideUp = ObjectAnimator.ofFloat(
+//                    splashScreenView,
+//                    View.TRANSLATION_Y,
+//                    0f,
+//                    -splashScreenView.height.toFloat()
+//                )
+//                slideUp.interpolator = AnticipateInterpolator()
+//                slideUp.duration = 500L
+//
+//                // Call SplashScreenView.remove at the end of your custom animation.
+//                slideUp.doOnEnd { splashScreenView.remove() }
+//
+//                // Run your animation.
+//                slideUp.start()
+//            }
+//        }
+    }
+}
---
+++ app/src/main/java/kr/co/dynamicmedia/test/MainViewModel.kt
@@ -0,0 +1,20 @@
+package kr.co.dynamicmedia.test
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    private val _complete = MutableLiveData<Boolean>()
+    val complete: LiveData<Boolean> get() = _complete
+
+    init {
+        _complete.value = false
+    }
+
+    fun updateComplete(){
+        _complete.value = true
+    }
+
+
+}
